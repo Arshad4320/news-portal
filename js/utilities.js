071a894a011ba5596ea8b73e7240a604425d181c
Arshad Ullah <mdarshadullah835@gmail.com>
2022-09-04 00:09:24 +0600

    modul part complated

---
+++ js/utilities.js
@@ -0,0 +1,9 @@
+// loading spinner
+const spinnerLading = isLoading => {
+    const spinner = document.getElementById("spinner");
+    if (isLoading) {
+        spinner.classList.remove('d-none')
+    } else {
+        spinner.classList.add('d-none')
+    }
+}
